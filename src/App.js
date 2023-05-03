445dbccd8e2822e7a6684cbc8f549a2e5b89b2d2
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-05-03 15:00:27 -0300

    Returning multiplerows within map in App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,12 @@ class App extends Component{
     const { posts } = this.state;
     return (
       <div className="App">
-        {posts.map(post => <h1 key={post.id}>{post.title}</h1>)}
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
