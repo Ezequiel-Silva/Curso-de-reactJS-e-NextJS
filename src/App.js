9c305ab648501e39201835a52f24e83f520fbe59
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-05-05 11:29:18 -0300

    Initial structure to asssemble the search in the api in App.js

--- src/App.js
+++ src/App.js
@@ -3,50 +3,15 @@ import { Component } from 'react';
 
 class App extends Component{
   state = {
-    counter: 0,
-    posts: [
-      {
-        id: 1,
-        title: 'O título 1',
-        body: 'O corpo 1'
-      },
-      {
-        id: 2,
-        title: 'O título 2',
-        body: 'O corpo 2'
-      },
-      {
-        id: 3,
-        title: 'O título 3',
-        body: 'O corpo 3'
-      }
-    ],
+    posts: []
   };
-  timeoutUpdate = null;
   componentDidMount(){
-    this.handleTimeout();
-  }
-  componentDidUpdate(){
-    this.handleTimeout();
-  }
-  componentWillUnmount(){
-    clearTimeout(this.timeoutUpdate);
-  }
-  handleTimeout = () => {
-    const { posts, counter } = this.state;
-    posts[0].title = 'O título mudou';
-    this.timeoutUpdate = setTimeout(() => {
-      this.setState({
-        posts,
-        counter: counter + 1
-      });
-    }, 3000);
+    //
   }
   render(){
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
     return (
       <div className="App">
-        <h1>{counter}</h1>
         {posts.map(post => (
           <div key={post.id}>
             <h1>{post.title}</h1>
