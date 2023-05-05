78bf15d15df824624274e5eb5c61e6ef13810ba0
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-05-04 23:27:22 -0300

    Using componentDidMount() in App.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import { Component } from 'react';
 
 class App extends Component{
   state = {
-    posts: [
-      {
-        id: 1,
-        title: 'O título 1',
-        body: 'O corpo 1'
-      },
-      {
-        id: 2,
-        title: 'O título 2',
-        body: 'O corpo 2'
-      },
-      {
-        id: 3,
-        title: 'O título 3',
-        body: 'O corpo 3'
-      }
-    ]
+    posts: []
   };
+  componentDidMount(){
+    this.setState({
+      posts: [
+        {
+          id: 1,
+          title: 'O título 1',
+          body: 'O corpo 1'
+        },
+        {
+          id: 2,
+          title: 'O título 2',
+          body: 'O corpo 2'
+        },
+        {
+          id: 3,
+          title: 'O título 3',
+          body: 'O corpo 3'
+        }
+      ]
+    });
+  }
   render(){
     const { posts } = this.state;
     return (
