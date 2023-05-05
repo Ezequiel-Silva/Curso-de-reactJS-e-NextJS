1b375a3fd90977aecda5514277ab4c189cc71037
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-05-05 08:53:40 -0300

    Handler the counter and changing the first title in App.js

--- src/App.js
+++ src/App.js
@@ -3,35 +3,40 @@ import { Component } from 'react';
 
 class App extends Component{
   state = {
-    posts: []
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'O corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'O corpo 3'
+      }
+    ]
   };
   componentDidMount(){
+    const { posts, counter } = this.state;
+    posts[0].title = 'O título mudou';
     setTimeout(() => {
       this.setState({
-        posts: [
-          {
-            id: 1,
-            title: 'O título 1',
-            body: 'O corpo 1'
-          },
-          {
-            id: 2,
-            title: 'O título 2',
-            body: 'O corpo 2'
-          },
-          {
-            id: 3,
-            title: 'O título 3',
-            body: 'O corpo 3'
-          }
-        ]
+        posts,
+        counter: counter + 1
       });
     }, 2000);
   }
   render(){
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
     return (
       <div className="App">
+        <h1>{counter}</h1>
         {posts.map(post => (
           <div key={post.id}>
             <h1>{post.title}</h1>
