dadef51ef9c2d030ba12c1ad8b53b8e6fd44a748
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-05-08 08:38:47 -0300

    Adding an img HTML tag to the API image in App.js

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component{
         <div className="posts">
           {posts.map(post => (
             <div className="post">
+              <img src={post.cover} alt={post.title}/>
               <div key={post.id} className="post-content">
                 <h1>{post.title}</h1>
                 <p>{post.body}</p>
