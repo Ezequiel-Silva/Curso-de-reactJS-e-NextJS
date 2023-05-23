e8f76b28b8d909e2e37a8c6d01efd39afc3f0945
ezequiel.ne@fatec.sp.gov.br <ezequiel.ne@fatec.sp.gov.br>
2023-05-22 22:24:12 -0300

    Using props in components/PostCard

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,13 +1,11 @@
-export const PostCard = (props) => {
-    console.log(props);
+export const PostCard = ({cover,title,id,body}) => {
     return(
-        <h1>Oi</h1>
-        // <div className="post">
-        //     <img src={post.cover} alt={post.title}/>
-        //     <div key={post.id} className="post-content">
-        //     <h1>{post.title}</h1>
-        //     <p>{post.body}</p>
-        //     </div>
-        // </div>
+        <div className="post">
+            <img src={cover} alt={title}/>
+            <div key={id} className="post-content">
+            <h1>{title}</h1>
+            <p>{body}</p>
+            </div>
+        </div>
     );
 }
