7b71c5ce473df2f22f294d7f26872962625ba6d2
ezequiel.ne@fatec.sp.gov.br <ezequiel.ne@fatec.sp.gov.br>
2023-05-22 22:33:48 -0300

    Reducing the return of the PostCard component

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,11 +1,10 @@
-export const PostCard = ({cover,title,id,body}) => {
-    return(
-        <div className="post">
-            <img src={cover} alt={title}/>
-            <div key={id} className="post-content">
-            <h1>{title}</h1>
-            <p>{body}</p>
-            </div>
+export const PostCard = ({cover,title,id,body}) => 
+(
+    <div className="post">
+        <img src={cover} alt={title}/>
+        <div key={id} className="post-content">
+        <h1>{title}</h1>
+        <p>{body}</p>
         </div>
-    );
-}
+    </div>
+);
