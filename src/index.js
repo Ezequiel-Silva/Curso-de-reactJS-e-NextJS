898ad21a80294bcbd651ca3c32a8638ebdf83a0a
ezequiel.ne@fatec.sp.gov.br <ezequiel.ne@fatec.sp.gov.br>
2023-06-04 14:52:20 -0300

    Modifying import in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styled/global-styled.css';
 import Home from './templates/Home';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,7 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Se você deseja começar a medir o desempenho em seu aplicativo, passe uma função
+// para registrar os resultados (por exemplo: reportWebVitals(console.log))
+// ou envie para um endpoint analítico. Saiba mais: https://bit.ly/CRA-vitals
 reportWebVitals();
