4f2cbcde82e2819a969d57a4284c0e0381b23263
ezequiel.ne@fatec.sp.gov.br <ezequiel.ne@fatec.sp.gov.br>
2023-06-10 23:09:16 -0300

    Modifying the key by id in components/Posts/index.jsx

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,9 +1,9 @@
-export const PostCard = ({cover,title,id,body}) => 
+export const PostCard = ({title,cover,body,id}) => 
 (
     <div className="post">
         <img src={cover} alt={title}/>
         <div className="post-content">
-        <h1>{title}</h1>
+        <h1>{title} {id}</h1>
         <p>{body}</p>
         </div>
     </div>
--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -4,7 +4,7 @@ export const Posts = ({posts}) => (
     <div className="posts">
         {posts.map(post => (
         <PostCard
-            key={post.id}
+            id={post.id}
             title={post.title}
             body={post.body}
             cover={post.cover} 
