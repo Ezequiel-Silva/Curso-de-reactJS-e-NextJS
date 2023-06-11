17edb8171157b465a0dbdd469c4cdcb4b78ce288
ezequiel.ne@fatec.sp.gov.br <ezequiel.ne@fatec.sp.gov.br>
2023-06-11 17:15:08 -0300

    Components/Button/styled.css styling

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,13 @@
+import './styled.css';
+
 import { Component } from 'react';
 
 export class Button extends Component {
     render(){
         const { text,onClick } = this.props;
         return (
-            <button 
+            <button
+                className='button' 
                 onClick={onClick}
             >{text}</button>
         )
