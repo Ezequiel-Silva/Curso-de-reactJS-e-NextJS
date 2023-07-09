f3aa1ed55291a59cd917f48ca1dc045236f7295d
ezequiel.ne@fatec.sp.gov.br <ezequiel.ne@fatec.sp.gov.br>
2023-07-09 00:10:02 -0300

    Changing the fetch http to https

--- src/utils/load-posts.js
+++ src/utils/load-posts.js
@@ -1,6 +1,6 @@
 export const loadPosts = async () => {
-    const postsResponse = fetch('http://jsonplaceholder.typicode.com/posts');
-    const photosResponse = fetch('http://jsonplaceholder.typicode.com/photos');
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
     const [posts,photos] = await Promise.all([postsResponse,photosResponse]);
     const postsJson = await posts.json();
     const photosJson = await photos.json();
